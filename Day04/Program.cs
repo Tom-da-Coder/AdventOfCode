f2cf236d9dc8c2e40997c57f0171e7ba5a71bc2b
Thomas Petersson <msdn@thomaspetersson.se>
2021-12-05 23:05:38 +0100

    Dag 5

---
+++ Day04/Program.cs
@@ -0,0 +1,118 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("AOC 2021-04A!");
+
+var rdr = new StreamReader("input.txt");
+
+List<int> callNums = rdr.ReadLine().Split(',').Select(s => int.Parse(s)).ToList();
+rdr.ReadLine();
+
+var Boards = new List<Board>();
+Board brd;
+
+while (!rdr.EndOfStream)
+{
+    Boards.Add(new Board(rdr));
+    if (rdr.EndOfStream)
+        break;
+    rdr.ReadLine();
+}
+
+// ****************************************************
+// Play
+
+int firstWin = 0;
+int lastWin = 0;
+
+foreach (var num in callNums)
+{
+    foreach (var board in Boards)
+    {
+        if (board.HasBingo) continue;
+        var bingo = board.AddNum(num);
+        switch (bingo)
+        {
+            case 0:
+                break;
+            case 1:
+            case 2:
+                lastWin = num * board.SumUnmarked();
+                if (firstWin == 0) firstWin = lastWin;
+                break;
+        }
+    }
+}
+
+Console.WriteLine(firstWin);
+Console.WriteLine(lastWin);
+
+// ****************************************************
+
+public struct Cell
+{
+    public int X;
+    public bool Checked;
+
+    public Cell(int x)
+    {
+        X = x;
+        Checked = false;
+    }
+}
+
+
+public class Board
+{
+    public Cell[,] Cells = new Cell[5, 5];
+    public bool HasBingo = false;
+
+    public Board(StreamReader rdr)
+    {
+        for (int r = 0; r < 5; r++)
+        {
+            var colsStr = rdr.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            var cols = colsStr.Select(q => int.Parse(q)).ToList();
+            for (int c = 0; c < 5; c++)
+                Cells[r, c] = new Cell(cols[c]);
+        }
+    }
+
+    public int AddNum(int num)
+    {
+        for (int r = 0; r < 5; r++)
+            for (int c = 0; c < 5; c++)
+                if (Cells[r, c].X == num)
+                {
+                    Cells[r, c].Checked = true;
+                    var bingo = CheckBingo(r, c);
+                    if (bingo != 0)
+                    {
+                        HasBingo = true;
+                        return bingo;
+                    }
+                }
+        return 0;
+    }
+
+    // Returns:
+    //  0 = no bingo
+    //  1 = bingo on row r
+    //  2 = bingo on column c
+    public int CheckBingo(int r, int c)
+    {
+        if (Enumerable.Range(0, 5).All(x => Cells[r, x].Checked))
+            return 1;
+        if (Enumerable.Range(0, 5).All(x => Cells[x, c].Checked))
+            return 2;
+        return 0;
+    }
+
+    public int SumUnmarked()
+    {
+        int sum = 0;
+        for (int r = 0; r < 5; r++)
+            for (int c = 0; c < 5; c++)
+                if (!Cells[r, c].Checked)
+                    sum += Cells[r, c].X;
+        return sum;
+    }
+}
---
+++ Day05/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Text.RegularExpressions;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("AOC 2021-05");
+
+var input = File.ReadAllLines("input.txt").Select(s => new Line(s))
+    //.Where(l => l.Horizontal || l.Vertical)
+    .ToList();
+
+var map = new byte[1000, 1000];
+
+foreach (var line in input)
+{
+    var diff = Math.Max(Math.Abs(line.dx), Math.Abs(line.dy));
+    var ddx = Math.Sign(line.dx);
+    var ddy = Math.Sign(line.dy);
+    var x = line.X1;
+    var y = line.Y1;
+
+    for (int i = 0; i <= Math.Abs(diff); i++)
+    {
+        map[x, y] += 1;
+        x += ddx;
+        y += ddy;
+    }
+}
+
+int res = 0;
+for (int x = 0; x < 1000; x++)
+    for (int y = 0; y < 1000; y++)
+        if (map[x, y] > 1)
+            res++;
+
+Console.WriteLine(res);
+
+public class Line
+{
+    public int X1;
+    public int X2;
+    public int Y1;
+    public int Y2;
+
+    public int dx => X2 - X1;
+    public int dy => Y2 - Y1;
+
+    public bool Horizontal => Y1 == Y2;
+    public bool Vertical => X1 == X2;
+
+    public Line(string s)
+    {
+        var match = Regex.Match(s, @"(\d+),(\d+) -> (\d+),(\d+)");
+        X1 = int.Parse(match.Groups[1].Value);
+        Y1 = int.Parse(match.Groups[2].Value);
+        X2 = int.Parse(match.Groups[3].Value);
+        Y2 = int.Parse(match.Groups[4].Value);
+        //if (X2 < X1)
+        //{
+        //    var q = X2;
+        //    X2 = X1;
+        //    X1 = q;
+        //}
+        //if (Y2 < Y1)
+        //{
+        //    var q = Y1;
+        //    Y1 = Y2;
+        //    Y2 = q;
+        //}
+    }
+}
