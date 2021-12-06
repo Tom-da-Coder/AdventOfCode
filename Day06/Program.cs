ca54c12bb13c7e78754e228b736b56534b0631b2
Thomas Petersson <thomas.petersson@rapidsakerhet.se>
2021-12-06 09:59:51 +0100

    Dag 6

---
+++ Day06/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("AOC 2021-06");
+
+var a = Apart();
+var b = Bpart();
+Console.WriteLine($"{a} {b}");
+
+int Apart() {
+    var fishes = File.ReadAllText("input.txt").Split(',').Select(s => int.Parse(s)).ToList();
+    for (int i = 0; i < 80; i++)
+    {
+        int siz = fishes.Count;
+        for (int j = 0; j < siz; j++)
+        {
+            if (fishes[j] == 0)
+            {
+                fishes[j] = 6;
+                fishes.Add(8);
+            }
+            else
+            {
+                fishes[j] -= 1;
+            }
+        }
+    }
+    return fishes.Count;
+}
+
+long Bpart()
+{
+    var fishes = File.ReadAllText("input.txt").Split(',').Select(s => int.Parse(s)).ToList();
+    long[] countOfAge = new long[9];
+    for (int i = 0;i < fishes.Count; i++)
+    {
+        countOfAge[fishes[i]]++;
+    }
+    for (int i = 0; i < 256; i++)
+    {
+        long zero = countOfAge[0];
+        for (int j = 0; j < 8; j++)
+            countOfAge[j] = countOfAge[j + 1];
+        countOfAge[8] = zero;
+        countOfAge[6] += zero;
+    }
+    return countOfAge.Sum();
+}
