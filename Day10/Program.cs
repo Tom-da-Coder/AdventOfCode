d29cae6033dba49b109d6346ea413d1e247537ea
Thomas Petersson <thomas.petersson@rapidsakerhet.se>
2021-12-10 09:14:25 +0100

    Creating Day10 and Template

---
+++ Day10/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.RegularExpressions;
+
+Console.WriteLine("AOC 2021-10");
+
+
+var input = File.ReadAllLines("input.txt");
+foreach (var line in input)
+{
+}
+Console.WriteLine(totCount);
