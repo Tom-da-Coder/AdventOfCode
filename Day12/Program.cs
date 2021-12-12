1a5405ce56a695ed31fc62c5a22c4d10c039bc2e
Thomas Petersson <msdn@thomaspetersson.se>
2021-12-13 00:52:50 +0100

    Dag 12 A klart

--- Day12/Program.cs
+++ Day12/Program.cs
@@ -5,7 +5,7 @@ int answer = 0;
 
 Dictionary<string, Node> NodeList = new Dictionary<string, Node>();
 
-var input = File.ReadAllLines("input2.txt");
+var input = File.ReadAllLines("input.txt");
 foreach (var line in input)
 {
     var nodes = line.Split('-').Select(x => (name: x, node: new Node(x))).Select(q => NodeList.TryAdd(q.name, q.node) ? q.node : NodeList[q.name]).ToList();
@@ -13,9 +13,23 @@ foreach (var line in input)
     nodes[1].Neighbours.Add(nodes[0]);
 }
 
-
+ScanOneNode(NodeList["start"]);
 
 Console.WriteLine(answer);
+
+
+void ScanOneNode(Node node)
+{
+    node.BinHere = true;
+    foreach (var neighbour in node.Neighbours)
+    {
+        if (neighbour.Name == "end")    // Mark full path
+            answer++;
+        else if (neighbour.BigCave || !neighbour.BinHere)
+            ScanOneNode(neighbour);
+    }
+    node.BinHere = false;
+}
 
 
 public class Node
