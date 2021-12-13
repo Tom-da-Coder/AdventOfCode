bcd6e6ebf5535e8ecbc4244106d16c763d59214c
Thomas Petersson <msdn@thomaspetersson.se>
2021-12-13 01:21:02 +0100

    Dag 12 B räknar fel

--- Day12/Program.cs
+++ Day12/Program.cs
@@ -2,17 +2,19 @@
 
 Console.WriteLine("AOC 2021-");
 int answer = 0;
+bool haveOneTwice = false;
+Stack<string> path = new Stack<string>();
 
 Dictionary<string, Node> NodeList = new Dictionary<string, Node>();
 
-var input = File.ReadAllLines("input.txt");
+var input = File.ReadAllLines("input3.txt");
 foreach (var line in input)
 {
     var nodes = line.Split('-').Select(x => (name: x, node: new Node(x))).Select(q => NodeList.TryAdd(q.name, q.node) ? q.node : NodeList[q.name]).ToList();
     nodes[0].Neighbours.Add(nodes[1]);
     nodes[1].Neighbours.Add(nodes[0]);
 }
-
+NodeList["start"].BinHere = 2;
 ScanOneNode(NodeList["start"]);
 
 Console.WriteLine(answer);
@@ -20,15 +22,24 @@ Console.WriteLine(answer);
 
 void ScanOneNode(Node node)
 {
-    node.BinHere = true;
+    path.Push(node.Name);
+    var twiceSave = haveOneTwice;
+    if (!node.BigCave && ++node.BinHere == 2)
+        haveOneTwice = true;
     foreach (var neighbour in node.Neighbours)
     {
         if (neighbour.Name == "end")    // Mark full path
+        {
             answer++;
-        else if (neighbour.BigCave || !neighbour.BinHere)
+            Console.WriteLine(string.Join('-', path.Reverse()));
+        }
+        else if (neighbour.BigCave || neighbour.BinHere < 2 && !haveOneTwice)
             ScanOneNode(neighbour);
     }
-    node.BinHere = false;
+    haveOneTwice = twiceSave;
+    if (!node.BigCave)
+        node.BinHere--;
+    path.Pop();
 }
 
 
@@ -36,7 +47,7 @@ public class Node
 {
     public readonly string Name;
     public List<Node> Neighbours = new List<Node>();
-    public bool BinHere = false;
+    public int BinHere = 0;
     public readonly bool BigCave;
 
     public Node(string name)
