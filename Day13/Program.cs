68f6b385e79453ae7efedf7e451d765371094605
Thomas Petersson <msdn@thomaspetersson.se>
2021-12-14 21:05:48 +0100

    Dag 13 startad

---
+++ Day13/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.RegularExpressions;
+
+Console.WriteLine("AOC 2021-13");
+int totCount = 0;
+
+var input = File.ReadAllLines("input.txt");
+foreach (var line in input)
+{
+    var match = Regex.Match(line, @"^(\d+,\d+)|(fold along (x|y)=(\d+))$");
+    if (!match.Success)
+        continue;
+
+}
+Console.WriteLine(totCount);
