6076b1280a7857078533c9ace4b3388308814d22
Thomas Petersson <msdn@thomaspetersson.se>
2021-12-16 00:52:16 +0100

    Dag 16 - initierad

---
+++ Day16/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("AOC 2021-");
+
+var input = File.ReadAllLines("input.txt");
+foreach (var line in input)
+{
+
+}
+Console.WriteLine(totCount);
