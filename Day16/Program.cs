b1e8175df32f6cd704105fb9e9d9e6ae3b08a37d
Thomas Petersson <msdn@thomaspetersson.se>
2021-12-17 01:03:01 +0100

    Dag 16 - startat

--- Day16/Program.cs
+++ Day16/Program.cs
@@ -2,9 +2,30 @@
 
 Console.WriteLine("AOC 2021-");
 
-var input = File.ReadAllLines("input.txt");
+
 foreach (var line in input)
 {
 
 }
 Console.WriteLine(totCount);
+
+public class BitStream
+{
+    private StreamReader rdr;
+    private byte part;
+    private int nBits;
+    public BitStream()
+    {
+        rdr = new StreamReader("input.txt");
+        part = (byte)rdr.Read();
+        nBits = 8;
+    }
+
+    public int GetBits(int n)
+    {
+        if (n > 32)
+            throw new Exception();
+        int ret = 0;
+
+    }
+}
