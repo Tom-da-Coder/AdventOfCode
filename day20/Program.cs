dcd92aa8f87eec0d5245c0b8ef402964f9c7af3c
Thomas Petersson <msdn@thomaspetersson.se>
2022-01-11 11:59:19 +0100

    Started day 20

---
+++ day20/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("AOC 2021-20");
+
+var input = File.ReadAllLines("input.txt");
+var map = input[0];
+
+var image = new byte[input[2].Length, input.Length];
+
+for (int y = 0; y < input.Length - 2; y++)
+{
+    var line = input[y + 2];
+    for (int x = 0; x < line.Length; x++)
+        image[x, y] = line[x]
+}
+foreach (var line in input.Skip(2))
+{
+
+}
+
+Console.WriteLine(totCount);
