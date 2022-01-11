5723297053aacce2bf671853b7396998bd66fc69
Thomas Petersson <msdn@thomaspetersson.se>
2022-01-11 18:30:20 +0100

    Dag 20 A korrekt svar

--- day20/Program.cs
+++ day20/Program.cs
@@ -9,31 +9,42 @@ var map = input[0];
 
 var image = input.Skip(2).ToList();
 
-ProcessImage();
-ProcessImage();
-
+PrintImage();
+ProcessImage(false); PrintImage();
+ProcessImage(true); PrintImage();
+PrintImage();
 var totCount = image.Sum(t => t.Count(c => c == '#'));
 Console.WriteLine(totCount);
 
-void ProcessImage()
+void ProcessImage(bool inv)
 {
     var newImage = new List<string>();
     for (int y = -1; y < image.Count() + 1; y++)
     {
         var sb = new StringBuilder();
         for (int x = -1; x < image[0].Count() + 1; x++)
-            sb.Append(Kernel(x, y));
+            sb.Append(Kernel(x, y, inv));
         newImage.Add(sb.ToString());
     }
     image = newImage;
 }
 
-char Kernel(int x, int y)
+char Kernel(int x, int y, bool inv)
 {
     int bin9 = 0;
     for (int y2 = y - 1; y2 <= y + 1; y2++)
         for (int x2 = x - 1; x2 <= x + 1; x2++)
-            if (x2 >= 0 && y2 >= 0 && x2 < image[0].Count() && y2 < image.Count() && image[y2][x2] == '#')
+        {
+            var fg = (x2 >= 0 && y2 >= 0 && x2 < image[0].Count() && y2 < image.Count());
+            if (fg && image[y2][x2] == '#' || !fg && inv)
                 bin9 += 1 << ((y + 1 - y2) * 3 + (x + 1 - x2));
+        }
     return map[bin9];
+}
+
+void PrintImage()
+{
+    for (int y = 0; y < image.Count(); y++)
+        Console.WriteLine(image[y]);
+    Console.WriteLine();
 }
